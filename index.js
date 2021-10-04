8bbcc2a17d8d76b3c82577e552a4fdcaa199ca36
root <root@aws-pdx-dev-shop-be-test.cd.local>
2021-10-04 08:27:27 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http')
+const hostname = '127.0.0.1';
+const fs = require('fs')
+const port = 3000
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'content-type': 'text/html' })
+  fs.createReadStream('index.html').pipe(res)
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server Running at http://${hostname}:${port}/`);
+});
