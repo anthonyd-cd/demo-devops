022072d6f4aaba62cd92817358bdd1274a99f681
anthonyd-cd <35614549+anthonyd-cd@users.noreply.github.com>
2021-10-04 13:43:26 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const hostname = '127.0.0.1';
 const fs = require('fs')
-const port = 3000
+const port = 8000
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
